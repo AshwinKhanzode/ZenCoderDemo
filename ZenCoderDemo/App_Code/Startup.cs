6df12171cd1dd9f8ed6be9bc164c183ab4daa5d0
Ashwin Khanzode <ashwin.khanzode@clariontech.com>
2025-05-30 10:14:09 +0530

    Initial commit

---
+++ ZenCoderDemo/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZenCoderDemo.Startup))]
+namespace ZenCoderDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
